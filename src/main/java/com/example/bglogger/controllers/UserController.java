a6f0013c033e7510316399b9c9e641923f3028de
Caleb C <kaichun24@gmail.com>
2026-08-31 16:24:21 -0700

    remove extra imports

--- src/main/java/com/example/bglogger/controllers/UserController.java
+++ src/main/java/com/example/bglogger/controllers/UserController.java
@@ -2,13 +2,10 @@ package com.example.bglogger.controllers;
 
 import java.net.URI;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.bglogger.dto.UserRegistrationDTO;
